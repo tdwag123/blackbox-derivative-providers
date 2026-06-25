e01ddb1dc115a7a48ced0cda56c5dfa9afa1ab55
Reiyah Jacobs <reiyahwj@gmail.com>
2026-06-24 17:29:51 -0700

    work in progress

--- FEM_Structure/1D_FEM.py
+++ FEM_Structure/1D_FEM.py
@@ -6,20 +6,22 @@ diffusion, elasticity, nonlinear elasticity, etc. It should only know how to
 integrate, assemble, solve. The physics supplies the constitutive law and source terms.
 '''
 
-
-
 # -------------------------------------------------------------------------------------------------
 # Create a uniform mesh with n elements on the interval I = [start, end] and define 
 # the corresponding space of continuous piecewise linear functions V_{h,0}.
 # ----------------------------------------------------------------------------------------------------
 def create_mesh(n: int, start: float, end: float):
-    # 
     nodes = np.linspace(start, end, n + 1) # generates an array of evenly spaced numbers over range I
 
-    return nodes
+    elements = np.array([
+        [i, i + 1]
+        for i in range(n)
+    ], dtype=int) # this array has data type int
+
+    return nodes, elements
 
 
-def quadrature():
+def gauss_quadrature():
     return 0
 
 
@@ -27,7 +29,7 @@ def shape_functions(x, nodes, n):
     return 0
 
 
-def assemble_Jacobian():
+def assemble_jacobian():
     return 0
 
 
@@ -35,7 +37,7 @@ def assemble_residual():
     return 0
 
 
-def boundary_conditions():
+def apply_boundary_conditions():
     return 0
 
 
