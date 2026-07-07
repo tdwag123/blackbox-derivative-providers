bddef59c683b2cdcf8681b27e799825185b56b57
Reiyah Jacobs <reiyah@unc.edu>
2026-07-06 17:43:32 -0700

    made progress on random fourier features

--- Methods/TabularDataMethods/RandomFeature.py
+++ Methods/TabularDataMethods/RandomFeature.py
@@ -1,19 +1,72 @@
 import numpy as np
+import pyrfm
 from sklearn.kernel_approximation import RBFSampler
 from sklearn.linear_model import SGDClassifier
+from GaussianProcessesWrap import KISSGPFluxST
 
 """
-"The RBFSampler constructs an approximate mapping for the radial basis function kernel, 
-also known as Random Kitchen Sinks [RR2007]. This transformation can be used to explicitly 
-model a kernel map, prior to applying a linear algorithm, for example a linear SVM"
+pyrfm: A library for random feature maps in Python, https://neonnnnn.github.io/pyrfm/.
+"""
 
-"The SGDClassifier estimator implements regularized linear models with stochastic gradient descent
-(SGD) learning: the gradient of the loss is estimated each sample at a time and the model is updated 
-along the way with a decreasing strength schedule (aka learning rate)."
+class RFFModel():
+    def __init__(
+            self, 
+            n_components=100, 
+            kernel='rbf', 
+            gamma='auto', 
+            use_offset=False, 
+            random_state=None,
+            n_data=900,
+            s_data,
+            T_data):
+        # RandomFourier is feature map object!
+        self.feature_map = pyrfm.random_feature.RandomFourier(n_components, kernel, gamma, use_offset, random_state)
+
+        rng = np.random.default_rng(0)
+        n_data = 900
+        s_data = rng.uniform(-2.0, 2.0, n_data)
+        T_data = rng.uniform(0.0, 3.0, n_data)
+
+        self.model = KISSGPFluxST(
+            s_data,
+        T_data,
+        q_data,
+        grid_size=40,
+        training_iter=70,
+        learning_rate=0.08,
+    )
 
 
-Random feature models are used in machine learning to approximate kernel methods. 
-They "use Monte Carlo approximations to kernel functions by randomly sampled feature maps"
+
+def random_fourier_features_1d(x, M):
+    """
+    idea of RFF: construct explicit feature map which is of dimension much lower than number of
+    observations, but with resulting inner product which approximates desired kernel function k(x,y)
+
+    x = array of shape (n_samples, n_features) holding training samples
+    y = array of shape (n_samples, ) holding target values
+    """
+
+    
+    return 0
+
+
+def example():
+    return 0
+
+if __name__ == "__main__":
+    example()
+
+
+"""
+"RBFSampler constructs approximate mapping for radial basis function kernel. 
+can be used to explicitly model a kernel map, prior to applying a linear algorithm"
+
+"SGDClassifier estimator implements regularized linear models with stochastic gradient descent
+(SGD) learning: gradient of loss is estimated each sample at a time and the model is updated 
+along the way with a decreasing strength schedule (aka learning rate)"
+
+RF models are used in machine learning to approximate kernel methods.
 
 Note: hyperparameters are set before training data; model parameters are learned from data
 
@@ -29,21 +82,9 @@ inputs:
     m = number of random features
     lambda = regularization strength
 
-pyrfm: A library for random feature maps in Python, https://neonnnnn.github.io/pyrfm/.
-
 Other references:
 https://pages.cs.wisc.edu/~yudongchen/cs839_sp22/5_random_features.pdf
 https://people.eecs.berkeley.edu/~brecht/papers/07.rah.rec.nips.pdf
 https://gregorygundersen.com/blog/2019/12/23/random-fourier-features/
 https://scikit-learn.org/stable/modules/generated/sklearn.kernel_approximation.RBFSampler.html
 """
-
-def random_fourier_features_1d(x, y):
-    """
-    x = array of shape (n_samples, n_features) holding training samples
-    y = array of shape (n_samples, ) holding target values
-    """
-
-    
-    return 0
-
