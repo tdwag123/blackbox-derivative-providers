44fdfc0ae371ef87be7e9b5e72329ce738606d92
Reiyah Jacobs <reiyah@unc.edu>
2026-07-07 11:05:33 -0700

    basic version of RFFModel done? + deleted MultilinearInterpolation

--- Methods/TabularDataMethods/MultilinearInterpolation.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-"""
-multinterp is used for multivariate interpolation: https://proceedings.scipy.org/articles/FGCJ9164
-"""
-
-class 
--- Methods/TabularDataMethods/RandomFeature.py
+++ Methods/TabularDataMethods/RandomFeature.py
@@ -1,7 +1,6 @@
 import numpy as np
 import pyrfm
 from sklearn.linear_model import Ridge
-from sklearn.linear_model import SGDClassifier  # used for classification (discrete values) so we will not be using this
 
 """
 pyrfm: A library for random feature maps in Python, https://neonnnnn.github.io/pyrfm/.
@@ -9,29 +8,57 @@ pyrfm: A library for random feature maps in Python, https://neonnnnn.github.io/p
 
 class RFFModel():
     """
-    Random Fourier Features model for constitutive law q = q(s,T) using RBF kernel and ridge regression
-    (s,T) -> random Fourier -> phi(s,T) -> ridge regression -> q
+    Random Fourier Features model for 1D constitutive law q = q(s) using RBF kernel and ridge regression
+    (s) -> random Fourier -> phi(s) -> ridge regression -> q
+
+    RFF is approximation of RBF kernel for large datasets to stop computer from running out of memory
     """
-    def __init__(self, n_components=100, kernel='rbf', gamma='auto', use_offset=False, random_state=None):
-        # RandomFourier is feature map object!
+
+    def __init__(self, n_components=100, kernel='rbf', gamma='auto', use_offset=False, random_state=None, alpha=1.0):
+        """
+        n_components: number of Random Fourier features
+
+        feature_map: turns X into phi(X)    Note: RandomFourier is feature map object!
+                                            https://neonnnnn.github.io/pyrfm/generated/pyrfm.random_feature.RandomFourier.html#pyrfm.random_feature.RandomFourier
+        model: learns q from phi(X)
+                                            https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.Ridge.html
+        """
+
         self.feature_map = pyrfm.random_feature.RandomFourier(
             n_components=n_components, 
             kernel=kernel, 
             gamma=gamma, 
-            use_offset=use_offset, 
+            use_offset=use_offset,
             random_state=random_state
         )
+        self.model = Ridge(alpha=alpha, fit_intercept=True, copy_X=True, max_iter=None, tol=0.0001, solver="auto")
 
-        self.model = Ridge(alpha=1, fit_intercept=True, copy_X=True, max_iter=None, tol=0.0001, solver="auto")
 
-    def fit():
+    def fit(self, X, y):
         """
-        x = array of shape (n_samples, n_features) holding training samples
-        y = array of shape (n_samples, ) holding target values
+        Fit linear ridge regression model on mapped feature matrix against target vector y
+        with regularization parameter alpha.
+
+        X = data matrix; array of shape (n_samples, n_features) holding training samples
+            Note: In the 1D case q = q(s), n_features = 1
+        y = target vector; 1D array of shape (n_samples, ) holding target values
         """
-        return 0
+
+        new_X = self.feature_map.fit_transform(X, y) # Fit feature_map to data, then transform it. Fits "transformer" to X, returns transformed version of X
+        self.model.fit(new_X, y) # Fits ridge regression model onto mapped feature matrix (the transformed version of X)
+
+        return self # feature_map is now fitted, model is now trained
+    
+
+    def predict(self, X):
+        """
+        Based on model formed by fit, predicts value for specific point.
+        """
+        new_X = self.feature_map.transform(X) # apply approximate feature map to input
+        return self.model.predict(new_X) # predict using linear model
 
 
+# ---------------------------------------------------------------------------------------------------------------------
 def example():
     return 0
 
@@ -46,6 +73,9 @@ can be used to explicitly model a kernel map, prior to applying a linear algorit
 "SGDClassifier estimator implements regularized linear models with stochastic gradient descent
 (SGD) learning: gradient of loss is estimated each sample at a time and the model is updated 
 along the way with a decreasing strength schedule (aka learning rate)"
+
+from sklearn.linear_model import SGDClassifier  # used for classification (discrete values) so we will not be using this
+
 
 RF models are used in machine learning to approximate kernel methods.
 
@@ -64,6 +94,8 @@ inputs:
     lambda = regularization strength
 
 Other references:
+https://proceedings.mlr.press/v70/avron17a.html
+
 https://pages.cs.wisc.edu/~yudongchen/cs839_sp22/5_random_features.pdf
 https://people.eecs.berkeley.edu/~brecht/papers/07.rah.rec.nips.pdf
 https://gregorygundersen.com/blog/2019/12/23/random-fourier-features/
