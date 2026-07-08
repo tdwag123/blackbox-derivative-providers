78af018f850716e22c9a339d719fa367cde89d8a
Reiyah Jacobs <reiyah@unc.edu>
2026-07-08 10:48:51 -0700

    testing different hyperparameters

--- Methods/TabularDataMethods/RandomFeature.py
+++ Methods/TabularDataMethods/RandomFeature.py
@@ -15,6 +15,7 @@ class RFFModel():
     RFF is approximation of RBF kernel for large datasets to stop computer from running out of memory
     """
 
+    # !!! n_components should be meaningfully smaller than n_data
     def __init__(self, n_components=400, gamma=0.1, random_state=None, alpha=1e-6):
         """
         n_components: number of Random Fourier features
@@ -122,7 +123,7 @@ def example_simple_1d():
         return np.sin(s) * (-1)
 
     rng = np.random.default_rng(0)
-    n_data = 2000
+    n_data = 5000
     s_data = rng.uniform(-2.0, 2.0, n_data)
     noise_scale = 0.035 * (1.0 + 0.25 * np.abs(s_data))
     q_data = q_true(s_data) + noise_scale * rng.standard_normal(n_data)
@@ -145,7 +146,7 @@ def example_simple_1d():
     print("1D Raw RMSE vs true derivative:", train_rmse_true_dq)
 
 
-    s_test = np.linspace(-2.0, 2.0, 400)
+    s_test = np.linspace(-2.0, 2.0, 5000)
     X_test = s_test.reshape(-1, 1)
     noise_scale_test = 0.035 * (1.0 + 0.25 * np.abs(s_test))
 
@@ -199,7 +200,7 @@ def example_2d():
         return base + oscillation + transition
 
     rng = np.random.default_rng(0)
-    n_data = 2000
+    n_data = 5000
     s_data = rng.uniform(-2.0, 2.0, n_data)
     T_data = rng.uniform(0.0, 3.0, n_data)
     noise_scale = 0.035 * (1.0 + 0.25 * np.abs(s_data))
