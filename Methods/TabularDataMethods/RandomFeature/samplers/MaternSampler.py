27a97a75d0ca03d08a0ddd619dc55a6e7f7d0f5e
Reiyah Jacobs <reiyah@unc.edu>
2026-07-24 11:55:06 -0700

    working on MaternSampler

--- Methods/TabularDataMethods/RandomFeature/samplers/MaternSampler.py
+++ Methods/TabularDataMethods/RandomFeature/samplers/MaternSampler.py
@@ -8,16 +8,35 @@ import numpy as np
 
 class MaternSampler:
     """
-    If we draw frequencies from a Student t's distribution, we are approximating a Matern kernel.
+    Approximate a Matern kernel feature map using random Fourier features that draw frequencies from Student t's distribution.
+
+    Parameters -------------------------------------------------------
+        n_components: number of random Fourier features. int, default=400
+            Number of Monte Carlo samples per original feature.
+            Equals the dimensionality of the computed feature space.
+
+        gamma: 'scale' or float, default=1.0
+            Parameter of RBF kernel: exp(-gamma * x^2).
+            If ``gamma='scale'`` is passed then it uses
+            1 / (n_features * X.var()) as value of gamma.
+
+        random_state: int, RandomState instance or None, default=None
+            Pseudo-random number generator to control the generation of the random
+            weights and random offset when fitting the training data.
+            Pass an int for reproducible output across multiple function calls.
+            See :term:`Glossary <random_state>`.
     """
     def __init__(
         self,
-        match_gaussian_variance,
+        match_gaussian_variance, 
         df, # controls tail heaviness
         n_components=400, 
         gamma=0.1,
         random_state=None
-    ):
+        ):
+
+        self.n_components = n_components
+        self.gamma = gamma
         
         
         if n_components <= 0:
