500a8037fd1a581880b0d2046cf00fe0a4060550
Abeeha Mirza <abeehamirza6@gmail.com>
2026-07-27 09:33:06 -0700

    exp

--- Testing/TabularTesting/comparison.py
+++ Testing/TabularTesting/comparison.py
@@ -337,7 +337,7 @@ if __name__ == "__main__":
     'linear_medium_noise.csv',
     '''
 
-    exp_name = "monotoneGPKPEPtest_noreg_2"
+    exp_name = "monotoneGPKPEPtest_noreg_3"
 
     regularization_options = [
         "",
